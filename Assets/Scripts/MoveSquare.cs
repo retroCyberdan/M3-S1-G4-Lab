91f39baeaf944de7739970a697bbfc9ba2514295
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-05-19 14:34:41 +0200

    Add exercise 1

--- Assets/Scripts/MoveSquare.cs
+++ Assets/Scripts/MoveSquare.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class MoveSquare : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        float speed = GetSpeed();
+        SetSpeed(speed);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float hori = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        if(hori != 0 || vert != 0)
+        {
+            Vector3 direction = new Vector3(hori, vert, 0);
+            transform.position += direction * (_speed * Time.deltaTime);
+        }
+    }
+
+    public float GetSpeed() => _speed;
+    public void SetSpeed(float speed)
+    {
+        if (speed <= 0)
+        {
+            _speed = Mathf.Max(10, speed);
+            Debug.Log($"Per poter saltare sugli altri livelli, la velocità non può essere 0!");
+        }
     }
 }
